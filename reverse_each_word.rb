6720322f27be2fc68accc4ee14deb9d85d104f6a
Victor Avila <avilavictor1570@gmail.com>
2020-07-17 17:44:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split
-  reverse_array = new_sentence.map do |word|
+  reverse_array = new_sentence.collect do |word|
     word.reverse
   end
   reverse_array.join (" ")
